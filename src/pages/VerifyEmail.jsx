2fde15069fcd546df1e0cc82074a2e4dc4764cc7
dexter1098 <emmadexter50@gmail.com>
2025-05-20 14:51:03 +0100

    feat: added resend api call

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -9,6 +9,20 @@ const VerifyEmail = () => {
   const { token } = useParams();
   const [errorMsg, setErrorMsg] = useState("");
   const [status, setStatus] = useState("verifying");
+  const [email, setEmail] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const handleResendEmail = async () => {
+    try {
+      const response = await axiosInstance.post("/auth/resend-email", {
+        email,
+      });
+      if (response.status === 200) {
+        setFeedback("Email Sent");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const checkToken = async () => {
     try {
       const response = await axiosInstance.post(`/auth/verify-email/${token}`, {
@@ -20,6 +34,7 @@ const VerifyEmail = () => {
     } catch (error) {
       setErrorMsg("Email Verification Failed");
       setStatus("error");
+      setEmail(error?.respomse?.ata?.email);
     }
   };
 
@@ -64,13 +79,14 @@ const VerifyEmail = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="w-full max-w-[505px] py-[29px] px-[26px] shadow-md text-center">
-        <MdCancel size={80} className="text-red-500 mx-auto"/>
+        <MdCancel size={80} className="text-red-500 mx-auto" />
+        <p className="bg-green-100 text-green-700 py-1.5 px-3 rounded-lg">{feedback}</p>
         <h1 className="text-xl lg:text-[30px] font-semibold my-3">
           Verification Failed
         </h1>
         <p className="text-[#666] mb-4">Invalid or expierd token</p>
 
-        <button className="w-full font-semibold rounded-xl bg-[#0c0c0c] text-white h-[56px]">
+        <button onClick={handleResendEmail} className="w-full font-semibold rounded-xl bg-[#0c0c0c] text-white h-[56px]">
           Resend verification email
         </button>
       </div>
